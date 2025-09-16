0a4aacb025bc50a515bb2b323407faa7080334f9
smashingtags <smashingtags@users.noreply.github.com>
2025-09-16 15:02:37 -0400

    fix: Update CI scripts to use HomeLabARR container registry
      - Change container references from ghcr.io/dockserver to ghcr.io/smashingtags
      - Update licensing headers from restrictive to MIT license
      - Rebrand from DockServer to HomeLabARR in all CI scripts
      - Fixes package update workflows that were failing due to wrong registry

--- .templates/ci/packages.sh
+++ .templates/ci/packages.sh
@@ -2,14 +2,14 @@
 ####################################
 # All rights reserved.              #
 # started from Zero                 #
-# Docker owned dockserver           #
-# Docker Maintainer dockserver      #
+# HomelabARR Container Bot           #
+# Maintainer HomelabARR CLI          #
 #####################################
 #####################################
-# THIS DOCKER IS UNDER LICENSE      #
-# NO CUSTOMIZING IS ALLOWED         #
-# NO REBRANDING IS ALLOWED          #
-# NO CODE MIRRORING IS ALLOWED      #
+# MIT LICENSE                       #
+# CUSTOMIZING IS ALLOWED             #
+# REBRANDING IS ALLOWED              #
+# CODE MIRRORING IS ALLOWED          #
 #####################################
 # shellcheck disable=SC2086
 # shellcheck disable=SC204
@@ -26,7 +26,7 @@ for i in ${folder[@]}; do
                --entrypoint /bin/sh \
                -v ${PWD}/$i/${app}:/tmp \
                -v ${PWD}/.templates/ci/pipeline.sh:/pipeline.sh \
-               ghcr.io/dockserver/${app}:latest -c 'apk add --quiet bash || apt install bash -yqq && chmod -cR 755 /pipeline.sh && bash /pipeline.sh'
+               ghcr.io/smashingtags/${app}:latest -c 'apk add --quiet bash || apt install bash -yqq && chmod -cR 755 /pipeline.sh && bash /pipeline.sh'
                docker stop CI-RUN-${app} && docker rmi CI-RUN-${app}
           fi
        fi
--- .templates/ci/pipeline.sh
+++ .templates/ci/pipeline.sh
@@ -2,14 +2,14 @@
 ####################################
 # All rights reserved.              #
 # started from Zero                 #
-# Docker owned dockserver           #
-# Docker Maintainer dockserver      #
+# HomelabARR Container Bot           #
+# Maintainer HomelabARR CLI          #
 #####################################
 #####################################
-# THIS DOCKER IS UNDER LICENSE      #
-# NO CUSTOMIZING IS ALLOWED         #
-# NO REBRANDING IS ALLOWED          #
-# NO CODE MIRRORING IS ALLOWED      #
+# MIT LICENSE                       #
+# CUSTOMIZING IS ALLOWED             #
+# REBRANDING IS ALLOWED              #
+# CODE MIRRORING IS ALLOWED          #
 #####################################
 # shellcheck disable=SC2086
 # shellcheck disable=SC2046
